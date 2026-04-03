168e3089b946301716e0f2ac45cb01d9f4e41ae3
Travis Miller <radezorack@gmail.com>
2026-04-03 14:32:29 -0400

    Removing Bun and Next

--- apps/web/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME !== "nodejs") {
-    return;
-  }
-
-  const { startPetGenerationWorker } = await import("@/src/lib/pets");
-  startPetGenerationWorker();
-}
